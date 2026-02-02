bd63e51199913c1740d1248463a65a84c20f0321
iamrohitlord <rohit.lord88@gmail.com>
2026-02-02 11:04:52 +0530

    initial commit

---
+++ test/features/demo/demo.feature
@@ -0,0 +1,11 @@
+Feature: Feature name
+
+    Feature Description
+    This is first feature file for my project
+
+    @demo
+    Scenario: Navigate to the login screen
+    
+    Given the user launches the CURA web app
+    When user clicks on Make appointment button
+    Then login page should be displayed
---
+++ test/features/step-definitions/demo.ts
@@ -0,0 +1,28 @@
+import { Given, When, Then } from "@wdio/cucumber-framework";
+/**
+ * Selectors
+ * Title:
+ * CURA Healthcare Service
+ *
+ * Make Appointment
+ * Link Text: Make Appointment
+ *
+ * Login Page url
+ * https://katalon-demo-cura.herokuapp.com/profile.php#login
+ *
+ *
+ *
+ */
+
+Given(/^the user launches the CURA web app$/, async function () {
+  await browser.url("https://katalon-demo-cura.herokuapp.com/");
+  await expect(browser).toHaveTitle("CURA Healthcare Service");
+});
+
+When(/^user clicks on Make appointment button$/, async function () {
+  await $("=Make Appointment").click();
+});
+
+Then(/^login page should be displayed$/, async function () {
+  await expect(browser).toHaveUrl(/.*#login/);
+});
